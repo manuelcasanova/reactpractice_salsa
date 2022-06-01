01631ccce3a56996412e5ee1b04692da0644fad4
Manuel Casanova <manucasanova@hotmail.com>
2022-06-01 10:08:31 -0700

    Add alternative removeStep functionality

--- src/Step.js
+++ src/Step.js
@@ -1,13 +1,13 @@
 export default function Step({step, removeStep}) {
 
-  function removeButtonClick(e) {
-    removeStep(step.id);
-  }
+  // function removeButtonClick(e) {
+  //   removeStep(step.id);
+  // }
 
   return (
     <div>
-      Id: {step.id} - Step: {step.title} - Level: {step.level}<br />
-      <button onClick={removeButtonClick}>Remove</button>
+      Id: {step.id} - Step: {step.title} - Level: {step.level}
+      <button onClick={() => removeStep(step.id)}>Remove</button>
     </div>
   )
 }
