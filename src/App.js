f65bb03a58b6640fc01bf1b69d32521e9d18f379
Manuel Casanova <manucasanova@hotmail.com>
2022-06-13 11:07:30 -0700

    Modify function addStep. Nested promise to get all the steps once we add a new one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import NewStepForm from './components/NewStepForm';
 // import EditStepForm from './components/EditStepForm';
@@ -11,66 +11,82 @@ function App() {
   // const [title, setTitle] = useState("");
   // const [level, setLevel] = useState("");
 
-  
-const stepsURL = 'http://localhost:8001/steps'
-// const levelsURL = 'http://localhost:8001/levels'
 
-useEffect(() => {
-  axios.get(stepsURL)
-  .then(function (res) {
-    setSteps([...res.data])
-  })
-}, [])
+  const stepsURL = 'http://localhost:8001/steps'
+  // const levelsURL = 'http://localhost:8001/levels'
+
+  useEffect(() => {
+    axios.get(stepsURL)
+      .then(function (res) {
+        setSteps([...res.data])
+      })
+  }, [])
 
 
-// useEffect(() => {
-//   Promise.all([
-//     axios.get(stepsURL),
-//   ]).then((all) => {
-//     setSteps(prev => ({...prev, steps:all[0].data}))
-//   })
-// }, [])
+  // useEffect(() => {
+  //   Promise.all([
+  //     axios.get(stepsURL),
+  //   ]).then((all) => {
+  //     setSteps(prev => ({...prev, steps:all[0].data}))
+  //   })
+  // }, [])
 
   function getNextId() {
     setId(prevId => prevId + 1)
     return id;
   }
-  
+
+  // function addStep(step) {
+  //   // console.log(step)
+
+  //   // setSteps([step, ...steps])
+  //   console.log('steps -->', steps)
+  //   return axios.post(`http://localhost:8001/steps/`,
+  //   step) //payload (see inspect)
+  //   .then(response => {
+  //     console.log("Step added:", response.data);
+  //     setSteps([...steps, response.data])
+  //     console.log('response data:', response.data)
+  //     return response
+  //   })
+  // } 
+
   function addStep(step) {
     // console.log(step)
-  
+
     // setSteps([step, ...steps])
-    // console.log('step -->', step, 'steps -->', steps)
+    console.log('steps -->', steps)
     return axios.post(`http://localhost:8001/steps/`,
-    step) //payload (see inspect)
-    .then(response => {
-      console.log("Step added:", response.data);
-      setSteps([...steps, response.data])
-      return response
-    })
-  } 
+      step) //payload (see inspect)
+      .then(() => {
+        axios.get(stepsURL)
+          .then(function (res) {
+            setSteps([...res.data])
+          })
+      })
+  }
 
   function editStep(stepId) {
     return axios.put(`http://localhost:8001/steps/edit/${stepId}`)
-    .then (response => {
-      console.log("Step edited:", response.data);
-      setSteps([...steps])
-    })
+      .then(response => {
+        console.log("Step edited:", response.data);
+        setSteps([...steps])
+      })
   }
 
   function deleteStep(stepId) {
     return axios.delete(`http://localhost:8001/delete/${stepId}`)
-    .then (res => {
-      setSteps(steps.filter(step => step.id !== stepId))
-      return res
-    })
+      .then(res => {
+        setSteps(steps.filter(step => step.step_id !== stepId))
+        return res
+      })
   }
-  
+
   return (
     <div className="App">
       <h3>Casino (Cuban Salsa) steps</h3>
-      <NewStepForm addStep={addStep} getNextId={getNextId}/>
-    {steps.map(step => <Step key={step.id} step={step} deleteStep={deleteStep} editStep={editStep}/>)}
+      <NewStepForm addStep={addStep} getNextId={getNextId} />
+      {steps.map(step => <Step key={step.step_id} step={step} deleteStep={deleteStep} editStep={editStep} />)}
 
     </div>
   );
