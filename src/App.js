2994c3552fbe02399634f5ab053808084d822db6
Manuel Casanova <manucasanova@hotmail.com>
2022-06-26 07:28:10 -0700

    Move components into Sidebar component. Move also refress setRefresh state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
 
 import './App.css';
-import NewStepForm from './components/NewStepForm';
-import StepList from './components/StepList';
+// import NewStepForm from './components/NewStepForm';
+// import StepList from './components/StepList';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
 
-  const [refresh, setRefresh] = useState(false);
+  // const [refresh, setRefresh] = useState(false);
 
   return (
     <div className="App">
-      <NewStepForm refresh={refresh} setRefresh={setRefresh}/>
-      <StepList refresh={refresh} setRefresh={setRefresh}/>
+      <Sidebar />
+      {/* <NewStepForm refresh={refresh} setRefresh={setRefresh}/>
+      <StepList refresh={refresh} setRefresh={setRefresh}/> */}
       
     </div>
   );
