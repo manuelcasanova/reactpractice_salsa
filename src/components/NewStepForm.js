052f3863a9723b2da20bf9ce282dde0c1280b555
Manuel Casanova <manucasanova@hotmail.com>
2022-06-29 20:30:03 -0700

    Remove hardcoded levels for dropdown. Now we get them from the db through the variable levels and maping through it

--- src/components/NewStepForm.js
+++ src/components/NewStepForm.js
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-export default function NewStepForm({level, setLevel, step, setStep}) {
+export default function NewStepForm({ level, setLevel, step, setStep }) {
 
   const [title, setTitle] = useState("");
-    //variable to hold the levels
-    const [levels, setLevels] = useState("Beginner")
-    const [steps, setSteps] = useState();
-    
+  //variable to hold the levels
+  const [levels, setLevels] = useState([])
+  const [steps, setSteps] = useState();
 
-    useEffect(() => {
-      axios.get(`${stepsURL}/levels`)
+
+  useEffect(() => {
+    axios.get(`${stepsURL}/levels`)
       .then(function (res) {
         setLevels([...res.data])
       })
-    }, []) 
-    
+  }, [])
+
+
   const stepsURL = 'http://localhost:8001'
 
   function addStep(step) {
     //Shows in console
     console.log("Step added:", step)
-  
+
     return axios.post(`${stepsURL}/steps`,
       step) //payload (see inspect)
       .then(() => {
         setStep([step])
       })
-  } 
+  }
 
   function changeStep(e) {
     setTitle(e.target.value);
@@ -54,67 +55,58 @@ export default function NewStepForm({level, setLevel, step, setStep}) {
   return (
     <div>
 
-{/* <!-- Button trigger modal --> */}
-<button type="button" className="fa-solid fa-circle-plus circle-plus" data-toggle="modal" data-target="#exampleModal">
-</button>
+      {/* <!-- Button trigger modal --> */}
+      <button type="button" className="fa-solid fa-circle-plus circle-plus" data-toggle="modal" data-target="#exampleModal">
+      </button>
 
-{/* <!-- Modal --> */}
-<div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Add new step</h5>
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className="modal-body">
+      {/* <!-- Modal --> */}
+      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Add new step</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
 
-      <label className="add_step_title" htmlFor="title">Step</label>
+              <label className="add_step_title" htmlFor="title">Step</label>
 
-      <input className="form-control" type="text" name="title" value={title} onChange={changeStep}/>
-    <p></p>
+              <input className="form-control" type="text" name="title" value={title} onChange={changeStep} />
+              <p></p>
 
-    <div className="level_input">
-    
-      <label className="add_step_title" htmlFor="title">Level</label>
-      <select className="form-control" value={level} onChange={e => setLevel(e.target.value)} >
-        {/* {Try to get these from the db so if we create more levels they appear automatically} */}
+              <div className="level_input">
+
+                <label className="add_step_title" htmlFor="title">Level</label>
+                <select className="form-control" value={level} onChange={e => setLevel(e.target.value)}  >
+                  {/* {Try to get these from the db so if we create more levels they appear automatically} */}
  
-        {/* {levels.map(level => 
-          <option>key={level.id} {level.title}</option>
- )} */}
+                   {levels.map((level) =>
+                    <option key={level.id}>
+                      {level.title}
+                    </option>
+                  )}  
+{/*App breaks when I reload the page. Uncaught TypeError levels.map is not a function. I comment out and in lines 85 to 88 and works again. FIXED by removing string from levels setLeves useState("Beginner")*/}
+                </select>
 
-        <option>Beginner</option>
-        <option>Intermediate</option>
-        <option>Intermediate 2</option>
-        <option>Advanced</option>
-        <option>Advanced 2</option>
-        <option>Hardcore</option>
+              </div>
 
-
-
-
-      </select>
-
-
-    </div>
-
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="Button_Close" data-dismiss="modal">Close</button>
+              {/* <button type="button" className="btn btn-primary">Save</button> */}
+              <button
+                className="Button_Submit"
+                type="Submit"
+                onClick={onSubmit}
+                /*to close modal after submission */
+                data-dismiss="modal"
+              >Add step</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="modal-footer">
-        <button type="button" className="Button_Close" data-dismiss="modal">Close</button>
-        {/* <button type="button" className="btn btn-primary">Save</button> */}
-        <button 
-        className="Button_Submit" 
-        type="Submit"
-        onClick={onSubmit} 
-        /*to close modal after submission */ 
-        data-dismiss="modal"
-        >Add step</button>
-      </div>
-    </div>
-  </div>
-</div>
 
 
     </div>
