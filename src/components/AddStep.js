8e140f0644fcf305a5230a22f943e3bbcf1e0f12
Manuel Casanova <manucasanova@hotmail.com>
2022-06-29 19:43:17 -0700

    Remove unused code and component

--- src/components/AddStep.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NewStepModal from './NewStepModal';
-
-
-export default function AddStep ({refresh, setRefresh}) {
-return (
-  <button className="fa-solid fa-circle-plus circle-plus"></button>
-)
-}
