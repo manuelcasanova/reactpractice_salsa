976a584ec6fa0bfb397959ac20c29ea8e66c7f97
Manuel Casanova <manucasanova@hotmail.com>
2022-06-29 19:36:08 -0700

    Change value level form control origin

--- src/components/EditStepForm.js
+++ src/components/EditStepForm.js
@@ -1,26 +1,49 @@
 import React, {Fragment, useState } from "react";
-import axios from 'axios'
+// import axios from 'axios'
 
 const stepsURL = 'http://localhost:8001/steps'
 
-const EditStepForm = ({step, refresh, setRefresh}) => {
- //console.log(step) //to see how the props gets here!
+const EditStepForm = ({step, level, setLevel}) => {
+//  console.log("Props pased to EditStepForm component: ", step)
+//  console.log("Props level: ", level)
+//  console.log("Props step: ", step)
 
 const [title, setTitle] = useState(step.step_title)
 const [level_id, setLevel_id] = useState(step.level_id)
 
 const updateStep = async(e) => {
   e.preventDefault();
+
   try {
-    const body = {title, level_id}
-    //Shows in the console
-    console.log("Step edited", body)
-    const response = await fetch(`${stepsURL}/edit/${step.step_id}`, {
+
+    const body = {title, level_id }
+    // const body = {title, level_id, level}
+    console.log("level_id before PUT", level_id)
+    console.log("body before PUT: ", body)
+
+    if (level === "Beginner") {
+      setLevel_id(1);
+    } else if (level === "Intermediate") {
+      setLevel_id(2);
+    } else if (level === "Intermediate 2") {
+      setLevel_id(3);
+    } else if (level === "Advanced") {
+      setLevel_id(4);
+    } else if (level === "Advanced 2") {
+      setLevel_id(5);
+    } else if (level === "Hardcore") {
+      setLevel_id(6);
+    }
+
+    const response = await fetch(`${stepsURL}/edit/${step.step_id}`, { //IAN response is assigned but not used
       method: "PUT",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify(body),
     })
-    setRefresh(!refresh)
+
+    // console.log("response: ",response);
+
+    
   } catch (err) {
     console.error(err.message)
   }
@@ -29,13 +52,18 @@ const updateStep = async(e) => {
   return <Fragment>
 
 {/* <!-- Button to Open the Modal --> */}
-<button className="Button_Edit" type="button" data-toggle="modal" data-target={`#id${step.step_id}`}>
+<button 
+className="Button_Edit" 
+type="button" 
+data-toggle="modal" 
+data-target={`#id${step.step_id}`}
+
+>
   Edit
 </button>
 
 {/* <!-- The Modal --> */}
-<div className="modal" id={`id${step.step_id}`} //I changed to modal1 from modal to understand CSS
-// onClick = {() => setTitle(step.step_title)}
+<div className="modal" id={`id${step.step_id}`} 
 >
   <div className="modal-dialog">
     <div className="modal-content">
@@ -44,7 +72,6 @@ const updateStep = async(e) => {
       <div className="modal-header">
         <h4 className="modal-title">Edit step</h4>
         <button type="button" className="close" data-dismiss="modal" 
-        // onClick = {() => setTitle(step.step_title)}
         >&times;
         </button>
       </div>
@@ -52,21 +79,40 @@ const updateStep = async(e) => {
       {/* <!-- Modal body --> */}
       <div className="modal-body">
       <label htmlFor="title">Step</label>
-        <input type="text" className="form-control" value={title} onChange={e => setTitle(e.target.value)}
-        />
-        {/* <input type="text" className="form-control" value={level_id} onChange={e => setLevel_id(e.target.value)}/> */}
+        <input 
+        type="text" 
+        className="form-control" 
+        value={title} 
+        onChange={e => setTitle(e.target.value)}/>
+       
         <label htmlFor="title">Level</label>
-        <select className="form-control" value={level_id} onChange={e => setLevel_id(e.target.value)}>
+        <select 
+        className="form-control" 
+        value={level} //Before I have level_id and setLevel_id SEE WITH IAN
+        onChange={e => setLevel(e.target.value)}
+        
+        > 
+        
+
+        {/* value={level_title} 
+        onChange={e => setLevel_title(e.target.value)}> */}
 
 
         {/* <select value={level} onChange={e => setLevel(e.target.value)} > */}
         {/* {Try to get these from the db so if we create more levels they appear automatically} */}
+        {/* <option></option> */}
         <option>Beginner</option>
         <option>Intermediate</option>
         <option>Intermediate 2</option>
         <option>Advanced</option>
         <option>Advanced 2</option>
         <option>Hardcore</option>
+        {/* <option>{level[0].title}</option>
+        <option>{level[1].title}</option>
+        <option>{level[2].title}</option>
+        <option>{level[3].title}</option>
+        <option>{level[4].title}</option>
+        <option>{level[5].title}</option> */}
       </select>
 
       </div>
@@ -74,15 +120,20 @@ const updateStep = async(e) => {
       {/* <!-- Modal footer --> */}
       
       <div className="modal-footer">
+      <button 
+      className="Button_Close" 
+      type="button" 
+      data-dismiss="modal" 
+      onClick = {() => setTitle(step.step_title)}
+        >Close</button>
         <button 
         className="Button_Edit" 
         type="button" 
         data-dismiss="modal" 
         onClick={e => updateStep(e)}
+        
         >Edit</button>
-        <button className="Button_Close" type="button" data-dismiss="modal" 
-        onClick = {() => setTitle(step.step_title)}
-        >Close</button>
+
       </div>
 
     </div>
